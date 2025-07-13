ffff3f6f9c33e4de95f0b60ff15b6ff913964cd4
Abdul Hadi <117abdulhadi@gmail.com>
2025-07-13 16:29:36 +0500

    Listener Class

--- project/src/engine/Listener.java
+++ project/src/engine/Listener.java
@@ -1,41 +1,4 @@
 package engine;
-
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.File;
-
-public class Listener implements ActionListener {
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        // Unimplemented method
-    }
-
-    /**
-     * Refresh the course list by clearing the model and adding new courses from
-     * the selected semester.
-     *
-     * @param gui the GUI object
-     */
-    public void refreshList(GUI gui) {
-        // Clear the model
-        gui.getCourseListModel().clear();
-        // Get the selected semester
-        String semester = (String) gui.getSemesterComboBox().getSelectedItem();
-        // No Semester is selected, do nothing
-        if (semester == null) {
-            return;
-        }
-        // Get the semester folder
-        File semesterFolder = new File(Utility.getBASE_DIR(), semester);
-        // List all the courses in the semester folder
-        File[] courses = semesterFolder.listFiles(File::isDirectory);
-        if (courses != null) {
-            // Add each course to the model
-            for (File course : courses) {
-                gui.getCourseListModel().addElement(course.getName());
-            }
-        }
-    }    
+public class Listener {
     
 }
